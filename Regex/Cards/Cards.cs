3db903bc42a50ecb9e165cd4d8b2973b3a3b3ffb
adrian043 <adrian1990@abv.bg>
2017-06-19 14:24:15 +0300

    Week 10

---
+++ Regex/Cards/Cards.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class Cards
+{
+    public static void Main()
+    {
+        var pattern = "(?<![0-9])(?:([2-9]|10)|(A|K|Q|J))+(S|H|D|C)";
+        var regex = new Regex(pattern);
+
+        var cardsInput = Console.ReadLine();
+
+        var matches = regex.Matches(cardsInput);
+
+        var cards = new List<string>();
+
+        foreach (Match match in matches)
+        {
+            cards.Add(match.Value);
+        }
+
+        Console.WriteLine(string.Join(", ", cards));
+    }
+}
+
